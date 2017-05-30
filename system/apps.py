b9d4e384bdbee22c3fa9223426e2196155a51b85
Articuno1234 <artbothelp@gmail.com>
2017-05-30 19:58:50 +1000

    Create apps.py

---
+++ system/apps.py
@@ -0,0 +1,13 @@
+INTRO = "This is where apps are stored! to make an app or to find an app go to `system/apps.py`"
+
+APPS = "No Apps!"
+
+"""When making an app make sure to add it to the list!
+   When you have made your app, in (boot.py) go to `def desktop()`
+   then under `if choice == "apps":` put
+   
+   my_app = apps.my_app()
+   if choice == "app_name":
+       clear_screen()
+       print(my_app)"""
+       
